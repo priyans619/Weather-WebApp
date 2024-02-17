b5aaafa20c2d755fe567a0747c839756c2bdecab
priyans619 <priyansh.2b@gmail.com>
2024-02-17 13:41:20 +0530

    Add searchLocation function for entering city name

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-
+import axios from "axios"
+import { useState } from "react"
 
 function App() {
-  
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
+
+  const searchLocation = (event) => {
+    if(event.key === "Enter") {
+      axios.get(url)
+        .then((res) => {
+          setData(res.data)
+          console.log(res.data)
+        })
+        .catch(error => {
+          console.error('Error fetching weather data:', error);
+        });
+        setLocation("")
+    }
+  }
   return (
     <div className="w-full h-full relative">
       <div className="text-center p-4">
@@ -12,6 +28,9 @@ function App() {
            border border-gray-200 text-gray-600 placeholder:text-gray-400
            focus:outline-none bg-white-600/100 shadow-md "
           placeholder="Enter Location"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDownCapture={searchLocation}
         />
       </div>
     </div>
