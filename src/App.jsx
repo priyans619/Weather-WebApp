300bcbafac9c524cb576dd29119def3302351ffd
priyans619 <priyansh.2b@gmail.com>
2024-02-18 00:14:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useState } from "react"
+import { useState , useEffect} from "react"
 import Search from "./components/Search";
 import Weather from "./components/Weather";
 
@@ -7,6 +7,8 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
+  const API_KEY = "ccc7a45803704b0e7d89b5a56001dee9";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${API_KEY}`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -27,7 +29,20 @@ function App() {
         });
       setLocation("")
     }
-  }
+  };
+
+  useEffect(() => {
+    const defaultCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&APPID=${API_KEY}`;
+    axios
+      .get(defaultCityUrl)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching default city weather data:", error);
+      });
+  }, [API_KEY]);
+
   return (
     <div className="w-screen h-screen bg-gradient ">
       <p className="text-3xl text-center font-semibold py-2">Welcome to Weather App</p>
