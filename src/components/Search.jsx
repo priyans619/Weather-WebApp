8e86622dfa925351f576be554c6a15cb4100cf80
priyans619 <priyansh.2b@gmail.com>
2024-02-17 22:37:49 +0530

    Add input to search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,10 @@ const Search = ({ location, setLocation, searchLocation }) => {
         className="py-2 px-3 md:w-[700px] w-full text-lg rounded-3xl
            border border-gray-200 text-gray-600 placeholder:text-gray-400
            focus:outline-none bg-white-600/100 shadow-md"
-        
+        placeholder="Enter Location"
+        value={location}
+        onChange={(event) => setLocation(event.target.value)}
+        onKeyDownCapture={searchLocation}
       />
     </div>
   );
