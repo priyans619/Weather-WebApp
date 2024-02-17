312a39768b8359698bb92d3fa18c6c40825c3e57
priyans619 <priyansh.2b@gmail.com>
2024-02-17 22:36:56 +0530

    Add Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Search = ({ location, setLocation, searchLocation }) => {
+  return (
+    <div className="text-center px-4 md:px-8 py-4 md:py-8">
+      <input
+        type="text"
+        className="py-2 px-3 md:w-[700px] w-full text-lg rounded-3xl
+           border border-gray-200 text-gray-600 placeholder:text-gray-400
+           focus:outline-none bg-white-600/100 shadow-md"
+        
+      />
+    </div>
+  );
+};
+
+export default Search;
